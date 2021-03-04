45b23151c4b8c7d1688f9e37b6fb78a97ec8d8b7
Marc <pansmarcjoe@t-online.de>
2021-03-04 16:58:31 +0100

    favicon and folder structure

---
+++ Program/logic.js
@@ -0,0 +1,32 @@
+//correctionB(wert)
+//correctionW(wert)
+//gradW=0;
+//gradB=0;
+//durationINms=0;
+//durationOUTms=0;
+
+//all in grad
+function resetCorr()
+{
+
+}
+function resetYellow()
+{
+
+}
+function minusB()
+{
+//durationOUTms
+}
+function plusB()
+{
+//durationOUTms
+}
+function minusW()
+{
+//durationINms
+}
+function plusW()
+{
+//durationINms
+}
