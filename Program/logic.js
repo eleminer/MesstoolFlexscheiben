50e3ecf69b8dff0aca857bf3c4888170adf26a12
Marc <pansmarcjoe@t-online.de>
2021-03-04 22:16:01 +0100

    counter überarbeitet

--- Program/logic.js
+++ Program/logic.js
@@ -16,11 +16,21 @@
 //2 = bclockwise
 //3 = wcounterclockwise
 //4= wclockwise
+////lastaction
+//lastactionIN = 0;
+//lastactionOUT = 0;
+//0=mit dem Uhrzeigersinn 1=gegen den Uhrzeigersinn 
 var noinput=0;
 
 
 function mathM(number)
 {
-
+if(durationINms!=0 && durationOUTms !=0)
+{
+    noinput=1;
+    //start
+    var speedB=111/durationOUTms;
+    var speedW=111/durationINms;
+}
 }
 
