d46b6a63d313d531d7d093067be93740919f2346
Marc <pansmarcjoe@t-online.de>
2021-03-05 14:59:38 +0100

    reset for correction not working, everything else works perfect

--- Program/logic.js
+++ Program/logic.js
@@ -23,54 +23,71 @@
 var noinput = 0;
 var differentinput = 0;
 var counter;
+var corrTempDiffW = 0;
+var corrTempDiffB = 0;;
 
 function timerM() {
     counter = setInterval(logicMath, 10);
 }
 
 function mathM(number) {
-    switch (number) {
-        case 1:
-            lastactionOUT = 1;
-            break;
-        case 2:
-            lastactionOUT = 0;
-            break;
-        case 3:
-            lastactionIN = 1;
-            break;
-        case 4:
-            lastactionIN = 0;
-            break;
-    }
-    if (lastactionIN == 0 && lastactionOUT == 1 || lastactionIN == 1 && lastactionOUT == 0) {
-        differentinput = 1;
-    } else {
-        differentinput = 0;
-    }
-    if (durationINms != 0 && durationOUTms != 0 && noinput == 0) {
-        noinput = 1;
-        timerM();
+    if (noinput == 0) {
+        switch (number) {
+            case 1:
+                lastactionOUT = 1;
+                break;
+            case 2:
+                lastactionOUT = 0;
+                break;
+            case 3:
+                lastactionIN = 1;
+                break;
+            case 4:
+                lastactionIN = 0;
+                break;
+        }
+        if (lastactionIN == 0 && lastactionOUT == 1 || lastactionIN == 1 && lastactionOUT == 0) {
+            differentinput = 1;
+        } else {
+            differentinput = 0;
+        }
+        if (durationINms != 0 && durationOUTms != 0) {
+            noinput = 1;
+            
+            corrTempDiffB = gradBCorr;
+            corrTempDiffW = gradWCorr;
+            timerM();
+        }
     }
 }
 
 function logicMath() {
+
     var speedB = 111 / durationOUTms; //wie viel grad pro ms
     var speedW = 111 / durationINms; //wie viel grad pro ms
-    if (lastactionIN == 0) {
-        gradW = gradW + (speedW * (360 / 111) * 0.10);
-    } else {
-        gradW = gradW - (speedW * (360 / 111) * 0.10);
-    }
-    if (lastactionOUT == 0) {
-        gradB = gradB + (speedB * (360 / 111) * 0.110);
-    } else {
-        gradB = gradB - (speedB * (360 / 111) * 0.10);
-    }
-    if(Math.round((gradW % 360) / (360 / 111)) == Math.round((gradB % 360) / (360 / 111)))
-    {
-        clearInterval(counter);
-    }
 
+
+
+    if (lastactionIN == 0) {
+        gradW = gradW + (speedW * (360 / 111) * 0.50);
+    } else {
+        gradW = gradW - (speedW * (360 / 111) * 0.50);
+    }
+    correctionW(gradW + corrTempDiffW);
+    if (lastactionOUT == 0) {
+        gradB = gradB + (speedB * (360 / 111) * 0.50);
+    } else {
+        gradB = gradB - (speedB * (360 / 111) * 0.50);
+    }
+    correctionB(gradB + corrTempDiffB);
+
+    if (Math.round((gradW % 360) / (360 / 111)) == Math.round((gradB % 360) / (360 / 111))) {
+        clearInterval(counter);
+        durationOUTms = 0;
+        durationINms = 0;
+        noinput = 0;
+        differentinput = 0;
+
+    }
 
 }
